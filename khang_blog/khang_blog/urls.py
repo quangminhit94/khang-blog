f2e5c6a1a433b67ebc698764fe8936a0cb1c688a
Minh Nguyen <nguyen_minh@tairapromote.co.jp>
2019-11-21 20:48:24 +0700

    update profile

--- khang_blog/khang_blog/urls.py
+++ khang_blog/khang_blog/urls.py
@@ -23,7 +23,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('',include('blog.urls')),
+
     path('register/', users_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
