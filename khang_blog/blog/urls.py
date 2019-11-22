c4f0b9b3147288f3ab17d3cc88ec56ab33982bfe
Minh Nguyen <nguyen_minh@tairapromote.co.jp>
2019-11-22 22:47:17 +0700

    pagination and filter user

--- khang_blog/blog/urls.py
+++ khang_blog/blog/urls.py
@@ -6,14 +6,16 @@ from .views import (
     PostCreateView,
     PostUpdateView,
     PostDeleteView,
+    UserPostListView,
 )
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('about/', views.about, name='blog-about'),
 
-    path('post/create/', PostCreateView.as_view(), name='post-create'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
 
+    path('post/create/', PostCreateView.as_view(), name='post-create'),
     path('post/<str:slug>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<str:slug>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<str:slug>/', PostDetailView.as_view(), name='post-detail'),
